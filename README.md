299f59a2b90b02e4c0e4c1936ced9ec3b785dc06
Minh Anh Le <minh.le5262@gmail.com>
2023-07-18 16:10:44 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# PyChain_Ledger
+# PyChain Ledger
+
+This challenge aims to create a secure, transparent, and decentralized ledger system that enables partner banks to conduct financial transactions and verify data integrity. By harnessing the power of blockchain, we can provide a tamper-resistant and auditable ledger that enhances trust among financial institutions.
+
+In the the basic `PyChain` ledger structure, I have made the following modifications:
+
+1. Create a new data class named `Record`.
+
+2. Modify the existing `Block` data class to store `Record` data.
+
+3. Add Relevant User Inputs to the Streamlit interface.
+
+4. Test the PyChain Ledger by Storing Records.
+
+## Streamlit Interface
+
+The following clip is a demo of using Streamlit and storing some mined blocks in my `PyChain` ledger, using the following steps:
+
+1. In the terminal, navigate to the folder of `pychain.py`, run the Streamlit application by using `streamlit run pychain.py`
+
+3. Enter values for the sender, receiver, and amount, and then click the Add Block button. Do this several times to store several blocks in the ledger.
+
+4. Verify the block contents and hashes in the Streamlit dropdown menu. 
+
+5. Test the blockchain validation process by using the web interface. 
+
+![demo](Images/recording.mov)
+
+## Blockchain
+
+The following is a screenshot of the Streamlit application page, which details a blockchain that consists of multiple blocks. 
+
+![blocks](Images/block.png)
+
+## Validity 
+
+The following is a screenshot of the Streamlit application page, which indicates the validity of the blockchain. 
+
+![validation](Images/validity.png)
---
+++ pychain.py
@@ -0,0 +1,135 @@
+# Imports
+import streamlit as st
+from dataclasses import dataclass
+from typing import Any, List
+import datetime as datetime
+import pandas as pd
+import hashlib
+
+# Create a Record Data Class
+@dataclass
+class Record:
+    sender: str
+    receiver: str
+    amount: float
+
+# Modify the Existing Block Data Class to Store Record Data
+@dataclass
+class Block:
+    record: Record
+    creator_id: int
+    prev_hash: str = "0"
+    timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
+    nonce: int = 0
+
+    def hash_block(self):
+        sha = hashlib.sha256()
+
+        record = str(self.record).encode()
+        sha.update(record)
+
+        creator_id = str(self.creator_id).encode()
+        sha.update(creator_id)
+
+        timestamp = str(self.timestamp).encode()
+        sha.update(timestamp)
+
+        prev_hash = str(self.prev_hash).encode()
+        sha.update(prev_hash)
+
+        nonce = str(self.nonce).encode()
+        sha.update(nonce)
+
+        return sha.hexdigest()
+
+@dataclass
+class PyChain:
+    chain: List[Block]
+    difficulty: int = 4
+
+    def proof_of_work(self, block):
+
+        calculated_hash = block.hash_block()
+
+        num_of_zeros = "0" * self.difficulty
+
+        while not calculated_hash.startswith(num_of_zeros):
+
+            block.nonce += 1
+
+            calculated_hash = block.hash_block()
+
+        print("Wining Hash", calculated_hash)
+        return block
+
+    def add_block(self, candidate_block):
+        block = self.proof_of_work(candidate_block)
+        self.chain += [block]
+
+    def is_valid(self):
+        block_hash = self.chain[0].hash_block()
+
+        for block in self.chain[1:]:
+            if block_hash != block.prev_hash:
+                print("Blockchain is invalid!")
+                return False
+
+            block_hash = block.hash_block()
+
+        print("Blockchain is Valid")
+        return True
+    
+# Adds the cache decorator for Streamlit
+@st.cache(allow_output_mutation=True)
+def setup():
+    print("Initializing Chain")
+    return PyChain([Block("Genesis", 0)])
+
+st.markdown("# PyChain")
+st.markdown("## Store a Transaction Record in the PyChain")
+
+pychain = setup()
+
+# Add Relevant User Inputs to the Streamlit Interface
+sender_input =  st.text_area('sender')
+
+# Add an input area where you can get a value for `receiver` from the user.
+receiver_input =  st.text_area('receiver')
+
+# Add an input area where you can get a value for `amount` from the user.
+amount_input =  st.text_area('amount')
+
+if st.button("Add Block"):
+    prev_block = pychain.chain[-1]
+    prev_block_hash = prev_block.hash_block()
+
+    # Update `new_block` 
+    new_block = Block(
+        record=Record(
+        sender=sender_input,
+        receiver=receiver_input,
+        amount=amount_input),
+        creator_id=42,
+        prev_hash=prev_block_hash
+    )
+
+    pychain.add_block(new_block)
+    st.balloons()
+
+st.markdown("## The PyChain Ledger")
+
+pychain_df = pd.DataFrame(pychain.chain).astype(str)
+st.write(pychain_df)
+
+difficulty = st.sidebar.slider("Block Difficulty", 1, 5, 2)
+pychain.difficulty = difficulty
+
+st.sidebar.write("# Block Inspector")
+selected_block = st.sidebar.selectbox(
+    "Which block would you like to see?", pychain.chain
+)
+
+st.sidebar.write(selected_block)
+
+if st.button("Validate Chain"):
+    st.write(pychain.is_valid())
